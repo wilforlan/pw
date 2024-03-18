ab0319f804e1d9bdc8b210e66af289e3b16477b4
Williams Isaac <williamscalg@gmail.com>
2024-03-18 04:10:23 +0100

    update bio

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
       'Williams Isaac - A software engineer, and a Founder.',
   },
   description:
-    'I’m Williams, a software designer and entrepreneur based in Lagos, Nigeria but working with people from over the world. I’m the founder of Fuddle.ai, where I am building an AI Nutritionist and First Engineer at Fireflies.ai, where we develop AI Meeting Assistant that will help revolutionize the how humans work and do meetings.',
+    'I’m Williams, a software designer and entrepreneur based in Lagos, Nigeria but working with people from over the world. I’m the founder of Fuddle.ai, where I am building an AI Nutritionist and First Engineer at Fireflies.ai, where we develop AI Meeting Assistant that will help revolutionize how humans work and do meetings.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
